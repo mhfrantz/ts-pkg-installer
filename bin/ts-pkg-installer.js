06e507433914bdda5636d5ebec3ce93c82f0746a
mfrantz <mfrantz@redsealnetworks.com>
2014-12-30 11:22:16 -0800

    Skeleton of the main script.

--- bin/ts-pkg-installer.js
+++ bin/ts-pkg-installer.js
@@ -1,15 +1,80 @@
 // ts-pkg-installer.ts
 ///<reference path="../typings/commander/commander.d.ts"/>
 ///<reference path="../typings/debug/debug.d.ts"/>
+///<reference path="../typings/glob/glob.d.ts"/>
 ///<reference path="../typings/node/node.d.ts"/>
+///<reference path="../typings/q/Q.d.ts"/>
 'use strict';
+var assert = require('assert');
 var commander = require('commander');
 var debug = require('debug');
+var path = require('path');
+var Q = require('q');
 var dlog = debug('ts-pkg-installer');
-function main() {
-    dlog('main');
-    // TODO
-}
+// Name of the configuration file for this program (default).
+var configFileDefault = 'tspi.json';
+// Name of the configuration file for the node package.
+var packageConfigFile = 'package.json';
+// Point to the standard location for exported module declarations.
+var exportDirGlob = path.join('lib', 'export', '*.d.ts');
+// Point to the location where typings will be exported.
+var typingsDir = path.join('..', '..', 'typings');
+// ## TypeScriptPackageInstaller
+// Used as the NPM postinstall script, this will do the following:
+// - Read configuration from tspi.json (or options.configFile)
+// - Wrap the exported declaration file(s)
+// - Copy the exported declaration file(s) to the "typings" directory
+var TypeScriptPackageInstaller = (function () {
+    function TypeScriptPackageInstaller(options) {
+        this.options = options;
+    }
+    // Main entry point to install a TypeScript package as an NPM postinstall script.
+    TypeScriptPackageInstaller.prototype.main = function () {
+        var _this = this;
+        dlog('main');
+        return this.readConfigFile().then(function () {
+            return _this.readPackageConfigFile();
+        }).then(function () {
+            return _this.wrapDeclaration();
+        }).then(function () {
+            return _this.copyExportedModules();
+        }).then(function () {
+            return _this.haulTypings();
+        });
+    };
+    // Read the configuration file for this utility.
+    TypeScriptPackageInstaller.prototype.readConfigFile = function () {
+        var configFile = this.options.configFile || configFileDefault;
+        // TODO
+        this.config = {};
+        return Q(null);
+    };
+    // Read the package configuration.
+    TypeScriptPackageInstaller.prototype.readPackageConfigFile = function () {
+        // TODO
+        this.packageConfig = {};
+        return Q(null);
+    };
+    // Wrap the exported declaration file based on the "main" file from package.json.
+    TypeScriptPackageInstaller.prototype.wrapDeclaration = function () {
+        assert(this.config);
+        // TODO
+        return Q(null);
+    };
+    // Copy exported modules into typings
+    TypeScriptPackageInstaller.prototype.copyExportedModules = function () {
+        assert(this.config);
+        // TODO
+        return Q(null);
+    };
+    // Incorporate typings from our own dependencies.
+    TypeScriptPackageInstaller.prototype.haulTypings = function () {
+        assert(this.config);
+        // TODO
+        return Q(null);
+    };
+    return TypeScriptPackageInstaller;
+})();
 // Parse command line arguments.
 commander.parse(process.argv);
 if (commander.args.length !== 0) {
@@ -17,6 +82,11 @@ if (commander.args.length !== 0) {
     commander.help();
 }
 else {
-    main();
+    var mgr = new TypeScriptPackageInstaller(commander.opts());
+    mgr.main().catch(function (err) {
+        dlog(err.toString());
+        process.stderr.write(__filename + ': ' + err.toString() + '\n');
+        process.exit(1);
+    });
 }
 //# sourceMappingURL=ts-pkg-installer.js.map
--- bin/ts-pkg-installer.ts
+++ bin/ts-pkg-installer.ts
@@ -1,18 +1,104 @@
 // ts-pkg-installer.ts
 ///<reference path="../typings/commander/commander.d.ts"/>
 ///<reference path="../typings/debug/debug.d.ts"/>
+///<reference path="../typings/glob/glob.d.ts"/>
 ///<reference path="../typings/node/node.d.ts"/>
+///<reference path="../typings/q/Q.d.ts"/>
 
 'use strict';
 
+import assert = require('assert');
 import commander = require('commander');
 import debug = require('debug');
+import glob = require('glob');
+import path = require('path');
+import Q = require('q');
 
 var dlog: debug.Debugger = debug('ts-pkg-installer');
 
-function main(): void {
-  dlog('main');
-  // TODO
+// Name of the configuration file for this program (default).
+var configFileDefault = 'tspi.json';
+// Name of the configuration file for the node package.
+var packageConfigFile = 'package.json';
+// Point to the standard location for exported module declarations.
+var exportDirGlob = path.join('lib', 'export', '*.d.ts');
+// Point to the location where typings will be exported.
+var typingsDir = path.join('..', '..', 'typings');
+
+// Command-line options, describing the structure of options in commander.
+interface IOptions {
+  configFile: string;
+}
+
+// Configuration data from tspm.json
+interface IConfig {
+}
+
+// Configuration data from package.json
+interface IPackageConfig {
+}
+
+// ## TypeScriptPackageInstaller
+// Used as the NPM postinstall script, this will do the following:
+// - Read configuration from tspi.json (or options.configFile)
+// - Wrap the exported declaration file(s)
+// - Copy the exported declaration file(s) to the "typings" directory
+class TypeScriptPackageInstaller {
+
+  private options: IOptions;
+  private config: IConfig;
+  private packageConfig: IPackageConfig;
+
+  constructor (options: IOptions) {
+    this.options = options;
+  }
+
+  // Main entry point to install a TypeScript package as an NPM postinstall script.
+  main(): Q.Promise<void> {
+    dlog('main');
+
+    return this.readConfigFile()
+      .then(() => { return this.readPackageConfigFile(); })
+      .then(() => { return this.wrapDeclaration(); })
+      .then(() => { return this.copyExportedModules(); })
+      .then(() => { return this.haulTypings(); });
+  }
+
+  // Read the configuration file for this utility.
+  private readConfigFile(): Q.Promise<void> {
+    var configFile = this.options.configFile || configFileDefault;
+    // TODO
+    this.config = {};
+    return Q(<void>null);
+  }
+
+  // Read the package configuration.
+  private readPackageConfigFile(): Q.Promise<void> {
+    // TODO
+    this.packageConfig = {};
+    return Q(<void>null);
+  }
+
+  // Wrap the exported declaration file based on the "main" file from package.json.
+  private wrapDeclaration(): Q.Promise<void> {
+    assert(this.config);
+    // TODO
+    return Q(<void>null);
+  }
+
+  // Copy exported modules into typings
+  private copyExportedModules(): Q.Promise<void> {
+    assert(this.config);
+    // TODO
+    return Q(<void>null);
+  }
+
+  // Incorporate typings from our own dependencies.
+  private haulTypings(): Q.Promise<void> {
+    assert(this.config);
+    // TODO
+    return Q(<void>null);
+  }
 }
 
 // Parse command line arguments.
@@ -22,5 +108,11 @@ if (commander.args.length !== 0) {
   process.stderr.write('Unexpected arguments.\n');
   commander.help();
 } else {
-  main();
+  var mgr = new TypeScriptPackageInstaller(<IOptions> commander.opts());
+  mgr.main()
+    .catch((err: Error) => {
+      dlog(err.toString());
+      process.stderr.write(__filename + ': ' + err.toString() + '\n');
+      process.exit(1);
+    });
 }
