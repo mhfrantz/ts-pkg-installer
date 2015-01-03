e9848bd3256fd7c89277a3613b06931586fa68bc
mfrantz <mfrantz@redsealnetworks.com>
2015-01-02 23:57:57 -0800

    Add 'Before' each scenario to set up temporary copy of test/data/repo.

--- features/step_definitions/installer-steps.ts
+++ features/step_definitions/installer-steps.ts
@@ -1,11 +1,77 @@
 // installer-steps.ts
+/// <reference path="../../typings/bluebird/bluebird.d.ts"/>
+/// <reference path="../../typings/chai/chai.d.ts"/>
+/// <reference path="../../typings/debug/debug.d.ts"/>
+/// <reference path="../../typings/mkdirp/mkdirp.d.ts"/>
+/// <reference path="../../typings/ncp/ncp.d.ts"/>
+/// <reference path="../../typings/node/node.d.ts"/>
+/// <reference path="../../typings/rimraf/rimraf.d.ts"/>
 
+import BluePromise = require('bluebird');
+import chai = require('chai');
+import debug = require('debug');
+import mkdirp = require('mkdirp');
+import ncp = require('ncp');
+import path = require('path');
+import rimraf = require('rimraf');
+
+// ### ICallback
+// Interface of the callback from Cucumber.js
 interface ICallback {
   (error?: string): void;
+  (error?: Error): void;
   pending(): void;
 }
 
+// ### IScenario
+// Interface of the scenario object from Cucumber.js
+interface IScenario {
+  getName(): string;
+}
+
+// ### IWorld
+// Interface to the "world" for these steps.
+interface IWorld {
+  scenarioName: string;
+  testOutputDir: string;
+}
+
 function wrapper() {
+  var dlog = debug('ts-pkg-installer:steps');
+  var expect = chai.expect;
+  var testDataRepo = path.join(process.cwd(), 'test', 'data', 'repo');
+  var rootOutputDir = path.join(process.cwd(), 'features', 'o');
+  var cwdSave = process.cwd();
+
+  // Set up a test area before each scenario.
+  this.Before(function (scenario: IScenario, callback: ICallback) {
+    var world = <IWorld> this;
+    world.scenarioName = scenario.getName();
+    expect(world.scenarioName).to.be.ok;
+
+    // Create a test output area for each scenario.
+    world.testOutputDir = path.join(rootOutputDir, world.scenarioName);
+
+    // Remove the entire test output directory
+    rimraf(world.testOutputDir, (error: Error): void => {
+      // Recreate it.
+      mkdirp(world.testOutputDir, (error: Error): void => {
+        // Copy the test data repo for this scenario.
+        dlog('Copying test/data/repo for scenario "' + world.scenarioName + '"');
+        ncp.ncp(testDataRepo, world.testOutputDir, (err: Error): void => {
+          // Change directory to the local copy of the test data repo.
+          process.chdir(world.testOutputDir);
+
+          callback(error);
+        });
+      });
+    });
+  });
+
+  // Restore the original cwd after each scenario.
+  this.After(function (callback: ICallback) {
+    process.chdir(cwdSave);
+  });
 
   this.Given(/^an NPM package "([^"]*)" written in TypeScript$/, function (pkg: string, callback: ICallback) {
     // Write code here that turns the phrase above into concrete actions
